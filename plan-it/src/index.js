22cda5e68186dfcf88d553aab6b0c978405054e8
Ishwant Singh <ishwant8@gmail.com>
2019-03-24 15:15:00 +0530

    23/40 done. working properly. debugging done

--- plan-it/src/index.js
+++ plan-it/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
@@ -26,4 +25,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
